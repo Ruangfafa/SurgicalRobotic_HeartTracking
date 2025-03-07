197213ae4a899d36fe2d26452570c8caf8e9e228
Ruangfafa <clksdysjsh0317@gmail.com>
2025-03-07 14:32:35 -0500

    Add codes for HaplyInverse3, set a good working space range for it

--- sources/Main.m
+++ sources/Main.m
@@ -1,12 +1,12 @@
 clear all;
 clc;
-
+addpath(fullfile(pwd, 'dependencies'));
 
 disp("Main: START");
 
 %========== Setup ==========
 rangeFinder = RangeFinder_setup("COM5",115200,8,1,2);
-meca = Meca500_setup(100);
+%meca = Meca500_setup(100);
 haply = HaplyInverse3_setup("COM8");
 %===========================
 
@@ -16,16 +16,20 @@ distance_ref = RangeFinder_getRange(rangeFinder); %Mark the current distance as 
 rangeFinder_difference = 0; %Initial value
 
 while isvalid(fig) %Loop if the window is there
-    distance = RangeFinder_getRange(rangeFinder); %Get the current distance
-
-    if(distance > 210 && distance < 590) %The distance limit for the Rangefinder
-        rangeFinder_difference = distance - distance_ref; %Get the distance difference
-    end
-    disp(rangeFinder_difference);
-        z_axis_WRF = 358 - rangeFinder_difference; %The default pos for z_axis_WRF is 308mm in Meca500
-        Meca500_writeline(meca,"MovePose",[190,0,z_axis_WRF,0,90,90]); %Move to the new location 
-
+     distance = RangeFinder_getRange(rangeFinder); %Get the current distance
+     
+     if(distance > 210 && distance < 590) %The distance limit for the Rangefinder
+         rangeFinder_difference = distance - distance_ref; %Get the distance difference
+     end
+     disp(rangeFinder_difference);
+         z_axis_WRF = 358 - rangeFinder_difference; %The default pos for z_axis_WRF is 308mm in Meca500
+    %     Meca500_writeline(meca,"MovePose",[190,0,z_axis_WRF,0,90,90]); %Move to the new location 
+    pos=HaplyInverse3_writeline(haply,"GetPos",0);
+    disp([pos(1),pos(2),pos(3)]);
     disp("main loop running...");
+    tic;
+    while (toc < 0.05)
+    end
 end %Close the window to exit the loop
 
 disp("Main: OVER");
