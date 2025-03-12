7404d7ef675e76d160b510957a920fa03dae0e4f
Ruangfafa <clksdysjsh0317@gmail.com>
2025-03-12 18:01:25 -0400

    Multy Thread #1

--- sources/System.m
+++ sources/System.m
@@ -1,40 +1,15 @@
-function System()
+function System(fileName, format)
+    if exist('systemLog.txt', 'file')
+        delete('systemLog.txt');
+    end
     diary('systemLog.txt');
     diary on;
-    clear all;
-    clc;
     addpath(fullfile(pwd, 'dependencies'));
     
-    disp("Main: START");
+    m = memmapfile(fileName, 'Format', format, 'Writable', true);
+    disp("System: START");
     
     %========= Globals =========
-    global systemOn;
-    systemOn = true;
-
-    global meca_moveX;
-    meca_moveX = 0; %1 / 0 / -1; Control the Meca500 moving on X-axis.
-    
-    global meca_moveY;
-    meca_moveY = 0; %1 / 0 / -1; Control the Meca500 moving on Y-axis.
-    
-    global meca_moveZ;
-    meca_moveZ = 0; %1 / 0 / -1; Control the Meca500 moving on Z-axis.
-    
-    global meca_moveZSpeed;
-    meca_moveZSpeed = 5; %int; Define the Meca500 moving speed in Z-axis.
-    
-    global meca_rangefinder_zAxisRelativelyStill;
-    meca_rangefinder_zAxisRelativelyStill = false; %True / False; Define the Meca500 will moving relatively still than the heart is z-axis.
-    
-    global haply_meca_moveOption;
-    haply_meca_moveOption = false; %True / False; Define the Meca500 is moving or not relating on Inverse3.
-    
-    global haply_meca_constraint;
-    haply_meca_constraint = false; %True / False; Define the Meca500 is moving with a given constraint, otherwise follow the tooltip.
-    
-    global haply_meca_doZero;
-    haply_meca_doZero = false; %True / False; To zero the Meca500 joints and Inverse3.
-    
     global haply_workSpace;
     haply_workSpace = [     -0.05   ,  0.135    ; 
                             -0.2    ,  -0.1     ; 
@@ -46,33 +21,30 @@ function System()
     %meca = Meca500_setup(100);
     %haply = HaplyInverse3_setup("COM9");
     %===========================
-    
-    
-    while systemOn
-        %disp("Main: Looping");
-        disp(systemOn);
-        tic;
-        if haply_meca_moveOption && haply_meca_constraint
+    tic;
+    while m.Data.systemOn
+        if m.Data.haply_meca_moveOption && m.Data.haply_meca_constraint
         end
     
-        if haply_meca_moveOption && ~haply_meca_constraint
+        if m.Data.haply_meca_moveOption && ~m.Data.haply_meca_constraint
         end
     
-        if (meca_moveX || meca_moveY) && ~haply_meca_moveOption
-            haply_meca_constraint = false;
+        if (m.Data.meca_moveX || m.Data.meca_moveY) && ~m.Data.haply_meca_moveOption
+            disp("x Pressed");
+            break;
             %Meca500_writeline(meca, "MoveLinRelWRF", [meca_moveX, meca_moveY, 0, 0, 0, 0]);
         end
     
-        if meca_moveZ
+        if m.Data.meca_moveZ
             %Meca500_writeline(meca, "MoveLinRelTRF", [0, 0, meca_moveZ, 0, 0, 0]);
         end
     
-        if haply_meca_doZero
+        if m.Data.haply_meca_doZero
             %Meca500_writeline(meca, "DoZero", 0);
             %HaplyInverse3_writeline(haply, "DoZero", 20);
         end
-        pause(1);
-        
     end
-    disp("Main: OVER");
+    
+    disp(toc);
+    disp("System: OVER");
     diary off;
